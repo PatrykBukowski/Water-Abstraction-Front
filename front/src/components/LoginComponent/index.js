9e356337b78438274f4332eada6bebd49bb0044e
Patryk Bukowski <patryk-bukowski@bullson.pl>
2020-12-29 20:42:02 +0100

    Added formik to register component

--- front/src/components/LoginComponent/index.js
+++ front/src/components/LoginComponent/index.js
@@ -1,51 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { authenticationService } from '../../services/AuthenticationService';
 import { Formik } from 'formik';
 
 const LoginComponent = props => {
-    // const [username, setUsername] = useState("")
-    // const [password, setPassword] = useState("")
-
-    // const handleUsernameInput = ({ target: { value } }) => setUsername(value)
-
-    // const handlePasswordInput = ({ target: { value } }) => setPassword(value)
-
-    // const handleLoginButton = () => {
-    //     authenticationService.login(username, password)
-    //         .then(
-    //             user => {
-    //                 const { from } = props
-    //                     || { from: { pathname: '/' } }
-    //                 props.history.push(from)
-    //             },
-    //             error => console.log(error)
-    //         )
-    // }
-
     return (
         <div>
             <Formik
                 initialValues={{ username: '', password: '' }}
-                onSubmit={(values, { setFieldError, setSubmitting }) => {
+                onSubmit={(values) => {
                     authenticationService.login(values.username, values.password)
                         .then(
                             user => {
                                 const { from } = props
                                     || { from: { pathname: '/' } }
                                 props.history.push(from)
-                            },
-                            error => {
-                                setFieldError("server", error)
-                                setSubmitting(false);
-                            }
+                            }, error => { }
                         )
                 }}
                 validate={values => {
                     const errors = {};
-                    if(!values.username){
+                    if (!values.username) {
                         errors.username = 'Required'
                     }
-                    if(!values.password){
+                    if (!values.password) {
                         errors.password = 'Required'
                     }
                     return errors;
@@ -59,51 +36,19 @@ const LoginComponent = props => {
                 }) => (
                     <form onSubmit={handleSubmit}>
                         <label htmlFor="username">Username</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            value={values.username}
-                            placeholder="Username"
-                            name="username"
-                            id="username"
-                        />
+                        <input type="text" onChange={handleChange}
+                            value={values.username} placeholder="Username"
+                            name="username" id="username" />
                         {errors.username && <p>{errors.username}</p>}
                         <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={handleChange}
-                            value={values.password}
-                        />
+                        <input type="password" name="password"
+                            onChange={handleChange} value={values.password} />
                         {errors.password && <p>{errors.password}</p>}
-                        {errors.server && <p>{errors.server}</p>}
                         <button type="submit">Zaloguj</button>
+                        <button type="button" onClick={() => props.register()}>Rejestracja</button>
                     </form>
                 )}
             </Formik>
-            {/* <div>
-                <input
-                    placeholder="Username"
-                    value={username}
-                    onChange={handleUsernameInput}
-                />
-            </div>
-            <div>
-                <input
-                    placeholder="Password"
-                    type="password"
-                    value={password}
-                    onChange={handlePasswordInput}
-                />
-            </div>
-            <div>
-                <button onClick={handleLoginButton}>
-                    Zaloguj
-                </button>
-                <button onClick={() => props.register()}>
-                    Rejestracja
-                </button>
-            </div> */}
         </div>
     )
 }
--- front/src/components/RegisterComponent/index.js
+++ front/src/components/RegisterComponent/index.js
@@ -1,63 +1,84 @@
 import React, { useState } from 'react'
 import { authenticationService } from '../../services/AuthenticationService'
+import { Formik } from 'formik';
 
 const RegisterComponent = ({ register }) => {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [email, setEmail] = useState("")
-    const [nationality, setNationality] = useState("")
-
-    const handleUsername = ({ target: { value } }) => setUsername(value)
-    const handlePassword = ({ target: { value } }) => setPassword(value)
-    const handleEmail = ({ target: { value } }) => setEmail(value)
-    const handleNationality = ({ target: { value } }) => setNationality(value)
-
-    const handleRegister = () => {
-        authenticationService.register(username, password, email, nationality)
-            .then(
-                response => { register() },
-                error => console.log(error)
-            )
-    }
-
     return (
         <div>
-            <div>
-                <input
-                    placeholder="Username"
-                    value={username}
-                    onChange={handleUsername}
-                />
-            </div>
-            <div>
-                <input
-                    placeholder="Password"
-                    value={password}
-                    onChange={handlePassword}
-                />
-            </div>
-            <div>
-                <input
-                    placeholder="Email"
-                    value={email}
-                    onChange={handleEmail}
-                />
-            </div>
-            <div>
-                <input
-                    placeholder="Nationality"
-                    value={nationality}
-                    onChange={handleNationality}
-                />
-            </div>
-            <div>
-                <button onClick={handleRegister}>
-                    Zarejestruj
-                </button>
-                <button onClick={() => register()}>
-                    Zogowanie
-                </button>
-            </div>
+            <Formik
+                initialValues={{
+                    username: '',
+                    password: '',
+                    email: '',
+                    nationality: '',
+                }}
+                onSubmit={(values) => {
+                    const { username, password, email, nationality } = values
+                    authenticationService.register(username, password, email, nationality)
+                        .then(
+                            response => { register() },
+                            error => console.log(error)
+                        )
+                }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.username) {
+                        errors.username = 'Username is required'
+                    } else if (values.useState.length < 4) {
+                        errors.username = 'Username is too short, minimum 4 chars is required'
+                    }
+                    if (!values.email) {
+                        errors.email = 'Email address is required'
+                    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                        errors.email = 'Invalid email address'
+                    }
+                    if (!values.password) {
+                        errors.password = 'Password is required'
+                    } else if (values.password.length < 6) {
+                        errors.password = 'Password is too short, minimum 6 chars is required'
+                    }
+                    if (!values.nationality) {
+                        errors.nationality = 'Nationality is required'
+                    } else if (values.password.length > 2) {
+                        errors.nationality = 'Nationality abbreviation contains 2 chars'
+                    }
+                    return errors
+                }}>
+                {({
+                    values,
+                    errors,
+                    handleChange,
+                    handleSubmit,
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">Username</label>
+                        <input type='text' onChange={handleChange}
+                            value={values.username} placeholder="Username"
+                            name="username" id="username" />
+                        {errors.username && <span>{errors.username}</span>}
+
+                        <label htmlFor="password">Password</label>
+                        <input type="password" onChange={handleChange}
+                            value={values.password} placeholder="Password"
+                            name="password" id="password" />
+                        {errors.password && <span>{errors.password}</span>}
+
+                        <label htmlFor="email">Email</label>
+                        <input type="email" onChange={handleChange}
+                            value={values.email} placeholder="Email"
+                            name="email" id="email" />
+                        {errors.email && <span>{errors.email}</span>}
+
+                        <label htmlFor="nationality">Nationality</label>
+                        <input type="text" onChange={handleChange}
+                            valye={values.nationality} placeholder="Nationality"
+                            name="nationality" id="nationality" />
+                        {errors.nationality && <span>{errors.nationality}</span>}
+                        <button type="submit">Zarejestruj</button>
+                        <button type="button" onClick={() => register()}>Logowanie</button>
+                    </form>
+                )}
+            </Formik>
         </div>
     )
 }
