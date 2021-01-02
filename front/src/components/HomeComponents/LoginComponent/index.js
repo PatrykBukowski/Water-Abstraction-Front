e8c9309e8d0e80c11259776332a97c798daefc8e
Patryk Bukowski <patryk-bukowski@bullson.pl>
2021-01-02 17:48:02 +0100

    Added style to register component

--- front/src/components/HomeComponents/LoginComponent/index.js
+++ front/src/components/HomeComponents/LoginComponent/index.js
@@ -1,20 +1,69 @@
-import React from 'react'
-import { Formik } from 'formik';
-import * as S from '../Styles/style'
+import { Formik } from 'formik'
+import styled from 'styled-components'
+import colors from '../../../utils/colors'
+
+const S = {}
+S.MainWrapper = styled.div`
+    background: ${colors.platinum};
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+S.FormWrapper = styled.div`
+    height: 600px;
+    width: 1000px;
+    background: ${colors.oxfordBlue};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+S.FormContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+S.Input = styled.input`
+    width: 320px;
+    height: 50px;
+    margin: 15px;
+    font-size: 32px;
+    border: none;
+    transition: .2s background;
+    background: ${(props) => props.error[props.name] ? colors.red : colors.platinum};
+    :focus{
+        outline: none;
+        background: ${colors.orangeWeb};
+    }
+`
+S.LoginButton = styled.button`
+    width: 320px;
+    height: 50px;
+    margin: 15px;
+    border: none;
+    background: ${colors.white};
+    font-size: 32px;
+    font-weight: bold;
+`
+S.RegisterButton = styled.p`
+    margin: 15px;
+    font-size: 20px;
+    color: ${colors.platinum};
+`
 
 const LoginComponent = props => {
     return (
-        <S.Container>
+        <S.MainWrapper>
             <Formik
                 initialValues={{ username: '', password: '' }}
                 onSubmit={(values) => {
                     props.login(values.username, values.password)
-                        .then(
-                            user => {
-                                const { from } = props
-                                    || { from: { pathname: '/' } }
-                                props.history.push(from)
-                            })
+                        .then(() => {
+                            const { from } = props
+                                || { from: { pathname: '/' } }
+                            props.history.push(from)
+                        })
                 }}
                 validate={values => {
                     const errors = {};
@@ -33,24 +82,20 @@ const LoginComponent = props => {
                     handleChange,
                     handleSubmit,
                 }) => (
-                    <S.FormContainer onSubmit={handleSubmit}>
-                        <S.Label htmlFor="username">Username</S.Label>
-                        <S.Input type="text" onChange={handleChange}
-                            value={values.username} placeholder="Username"
-                            name="username" id="username" />
-                        <S.ErrorMessage>{errors.username && <S.ErrorParagraph>{errors.username}</S.ErrorParagraph>}</S.ErrorMessage>
-                        <S.Label htmlFor="password">Password</S.Label>
-                        <S.Input type="password" name="password"
-                            onChange={handleChange} value={values.password} placeholder="Password" />
-                        <S.ErrorMessage>{errors.password && <S.ErrorParagraph>{errors.password}</S.ErrorParagraph>}</S.ErrorMessage>
-                        <S.ButtonContainer>
-                            <S.Button type="submit">Zaloguj</S.Button>
-                            <S.Button type="button" onClick={() => props.register()}>Rejestracja</S.Button>
-                        </S.ButtonContainer>
-                    </S.FormContainer>
+                    <S.FormWrapper>
+                        <S.FormContainer onSubmit={handleSubmit}>
+                            <S.Input type="text" onChange={handleChange}
+                                value={values.username} placeholder="Username"
+                                name="username" id="username" error={errors} />
+                            <S.Input type="password" name="password" error={errors}
+                                onChange={handleChange} value={values.password} placeholder="Password" />
+                            <S.LoginButton type="submit">LOGIN</S.LoginButton>
+                            <S.RegisterButton onClick={() => props.register()}>REGISTER</S.RegisterButton>
+                        </S.FormContainer>
+                    </S.FormWrapper>
                 )}
             </Formik>
-        </S.Container>
+        </S.MainWrapper>
     )
 }
 
